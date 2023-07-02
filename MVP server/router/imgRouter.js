ecd5e411d44a35fb34928a46e00fe774369c3a12
Emre Guler <mfbg1985@gmail.com>
2023-07-02 03:39:52 +0300

    img front

--- MVP server/router/imgRouter.js
+++ MVP server/router/imgRouter.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const imgController = require("../controllers/imgController.js");
 
 router.get("/", imgController.getAllImg);
-router.post("/", imgController.postOneImg);
+router.post("/uploads", imgController.postOneImg);
 // router.delete("/:id", imgController.deleteImg);
 // router.put("/:id", imgController.updateImg);
 // router.get("/:userId", imgController.getAllUserImg);
